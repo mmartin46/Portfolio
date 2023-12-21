1ad307e9695a6111494e61ac339e5eacef7bf3c3
mmartin46 <mmartin46@angelo.edu>
2023-12-21 10:33:09 -0600

    fixed html validator issues

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* global document */
-/* jslint browser */
+/*jslint browser */
 // script.js
 
 
@@ -23,34 +23,52 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    Object.values(allSections).forEach(function (sectionId, index) {
-        let contentDivString = sectionId + "_content";
-        let contentDiv = document.getElementById(contentDivString);
+    // Initalizes the sections to not be selected
+    function initializeSections(allSections) {
+        Object.values(allSections).forEach(function (sectionId, index) {
+            let contentDivString = sectionId + "_content";
+            let contentDiv = document.getElementById(contentDivString);
 
-        // Start by setting the default section to the about me section.
-        if (contentDiv) {
-            contentDiv.style.display = index === 0 ? "block" : "none";
-        }
-    });
+            // Start by setting the default section to the about me section.
+            if (contentDiv) {
+                contentDiv.style.display = "none";
+            }
+        });
+    }
+
+    initializeSections(allSections);
 
     function whichSection(clickedSection) {
         let contentDivString = clickedSection + "_content";
         let contentDiv = document.getElementById(contentDivString);
 
         // If the section has already been clicked
-        if (contentDiv.style.display === "block") {
+        if (contentDiv.classList.contains("active")) {
             return;
         }
 
         Object.values(allSections).forEach(function (sectionId) {
-            console.log(sectionId + '_content');
             if (contentDiv) {
-                handleMatch(contentDiv, sectionId, clickedSection);
+                if (sectionId === clickedSection) {
+                    console.log(sectionId + "_content");
+                    handleMatch(contentDiv, sectionId, clickedSection);
+                }
             }
         });
     }
 
     function handleMatch(contentDiv, sectionId, clickedSection) {
-        contentDiv.style.display = sectionId === clickedSection ? "block" : "none";
+        Object.values(allSections).forEach(function (section) {
+            let currContentDiv = document.getElementById(section + "_content");
+            if (currContentDiv) {
+                currContentDiv.style.display = "none";
+            }
+
+            contentDiv.style.display = "block";
+            document.getElementById(section + "_content").classList.remove("active");
+        });
+
+        contentDiv.style.display = "block";
+        contentDiv.classList.add("active");
     }
 });
